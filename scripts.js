0613dfe13253ef2aae00959e4cead270403730e7
Rodrigo Emanuel <drigosantos@gmail.com>
2020-02-13 13:48:04 -0300

    Botão maximizar do modal

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
 
 for (let card of cards) {
     card.addEventListener("click", function(){
@@ -12,4 +13,10 @@ for (let card of cards) {
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove("active");
     modalOverlay.querySelector("iframe").src="";
+})
+
+document.querySelector('.maxi-modal').addEventListener("click", function(event){
+    event.preventDefault();
+    modal.classList.remove("modal");
+    modal.classList.add("maxi-modal-active");   
 })
